34868e5df310a1b726138fc27c3066deed520bdd
Raycho Ivanov <rastiv.bg@gmail.com>
2026-05-01 08:43:27 +0300

    add Tab component

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -7,6 +7,7 @@ const HomePage = () => {
   return (
     <div className={styles.pageWrapper}>
       <Header />
+      <main className={styles.content}>asd</main>
       <Footer />
     </div>
   );
--- src/shares/ui/index.ts
+++ src/shares/ui/index.ts
@@ -2,5 +2,6 @@ import AppIcon from "./AppIcon/AppIcon";
 import Button from "./Button/Button";
 import Input from "./Input/Input";
 import Spinner from "./Spinner/Spinner";
+import Tabs from "./Tabs/Tabs";
 
-export { Button, Input, AppIcon, Spinner };
+export { Button, Input, AppIcon, Spinner, Tabs };
