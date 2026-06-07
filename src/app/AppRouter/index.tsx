c643436fd4f72b5085bae3214f8b7b9a15ea9512
Raycho Ivanov <rastiv.bg@gmail.com>
2026-06-07 21:18:08 +0300

    unpdate AppRouter

--- src/app/AppRouter/index.tsx
+++ src/app/AppRouter/index.tsx
@@ -1,46 +1,30 @@
-import { Suspense } from "react";
 import { Route, Routes } from "react-router";
-import { PageLoader } from "@/shared/components/PageLoader";
 import { MainLayout } from "@/shared/components/layout/MainLayout";
-import { routeConfig, type AppRouteProps } from "./routerConfig";
-import { RequireAuth, RequireGuest } from "./routerGuards";
+import { routeConfig } from "./routerConfig";
+import { RequireGuest, RequireAuth } from "./routerGuards";
 
 export const AppRouter = () => {
-  const protectedRoutes = routeConfig.filter((route) => route.authOnly);
-  const standaloneRoutes = routeConfig.filter((route) => !route.authOnly);
-
-  <Route element={<MainLayout />}>asd</Route>;
-
-  // return (
-  //   <Routes>
-  //     {protectedRoutes.map(({ path, element }: AppRouteProps) => (
-  //       <Route key={path} path={path} element={<MainLayout />}>
-  //         <Route
-  //           index
-  //           element={
-  //             <RequireAuth>
-  //               <Suspense fallback={<PageLoader />}>{element}</Suspense>
-  //             </RequireAuth>
-  //           }
-  //         />
-  //       </Route>
-  //     ))}
-
-  //     {standaloneRoutes.map(({ path, element, guestOnly }: AppRouteProps) => {
-  //       const content = (
-  //         <Suspense fallback={<PageLoader />}>{element}</Suspense>
-  //       );
-
-  //       return (
-  //         <Route
-  //           key={path}
-  //           path={path}
-  //           element={
-  //             guestOnly ? <RequireGuest>{content}</RequireGuest> : content
-  //           }
-  //         />
-  //       );
-  //     })}
-  //   </Routes>
-  // );
+  return (
+    <Routes>
+      <Route element={<MainLayout />}>
+        {routeConfig.map(({ path, element, authOnly, guestOnly }) => {
+          return (
+            <Route
+              key={path}
+              path={path}
+              element={
+                guestOnly ? (
+                  <RequireGuest>{element}</RequireGuest>
+                ) : authOnly ? (
+                  <RequireAuth>{element}</RequireAuth>
+                ) : (
+                  element
+                )
+              }
+            />
+          );
+        })}
+      </Route>
+    </Routes>
+  );
 };
